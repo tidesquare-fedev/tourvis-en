5bcbf099a99ad187725c3baf42180b6e78886de7
unknown <lkil@tidesquare.com>
2025-09-02 12:11:42 +0900

    버그 수정

--- src/features/tour/components/TourHeroSection.tsx
+++ src/features/tour/components/TourHeroSection.tsx
@@ -120,7 +120,9 @@ export function TourHeroSection({ tourData, tour, starColor, onScrollToReviews, 
           <div className="order-1 lg:order-1">
             <div className="relative group">
               <img
-                src={allImages[currentMainImageIndex]?.file_url || allImages[currentMainImageIndex]}
+                src={typeof allImages[currentMainImageIndex] === 'string' 
+                  ? allImages[currentMainImageIndex] 
+                  : allImages[currentMainImageIndex]?.file_url || allImages[currentMainImageIndex]}
                 alt={tour.title}
                 className="w-full h-[300px] sm:h-[400px] lg:h-[600px] object-cover rounded-none lg:rounded-2xl shadow-lg cursor-pointer"
                 onClick={handleMainImageClick}
@@ -156,7 +158,7 @@ export function TourHeroSection({ tourData, tour, starColor, onScrollToReviews, 
                       onClick={() => handleThumbnailClick(index)}
                     >
                       <img
-                        src={image?.file_url || image}
+                        src={typeof image === 'string' ? image : image?.file_url || image}
                         alt={`${tour.title} ${index + 1}`}
                         className={`w-full h-20 object-cover rounded-lg shadow-sm group-hover:shadow-md transition-all duration-200 ${
                           currentMainImageIndex === index ? 'ring-2 ring-blue-500' : ''
@@ -278,7 +280,7 @@ export function TourHeroSection({ tourData, tour, starColor, onScrollToReviews, 
                   onClick={() => handleThumbnailClick(index)}
                 >
                   <img
-                    src={image?.file_url || image}
+                    src={typeof image === 'string' ? image : image?.file_url || image}
                     alt={`${tour.title} ${index + 1}`}
                     className={`w-full h-20 sm:h-24 lg:h-32 object-cover rounded-lg shadow-sm group-hover:shadow-md transition-all duration-200 ${
                       currentMainImageIndex === index ? 'ring-2 ring-blue-500' : ''
@@ -328,7 +330,9 @@ export function TourHeroSection({ tourData, tour, starColor, onScrollToReviews, 
               {/* Main Image Container */}
               <div className="relative h-full flex items-center justify-center bg-gray-50">
                 <img
-                  src={allImages[selectedImageIndex]?.file_url || allImages[selectedImageIndex]}
+                  src={typeof allImages[selectedImageIndex] === 'string' 
+                    ? allImages[selectedImageIndex] 
+                    : allImages[selectedImageIndex]?.file_url || allImages[selectedImageIndex]}
                   alt={`${tour.title} ${selectedImageIndex + 1}`}
                   className="max-w-full max-h-full object-contain"
                 />
@@ -379,7 +383,7 @@ export function TourHeroSection({ tourData, tour, starColor, onScrollToReviews, 
                       }`}
                     >
                       <img
-                        src={image.file_url || image}
+                        src={typeof image === 'string' ? image : image?.file_url || image}
                         alt={`${tour.title} ${index + 1}`}
                         className="w-full h-full object-cover"
                       />
